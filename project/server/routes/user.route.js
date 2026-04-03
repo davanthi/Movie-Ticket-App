d7e054016f5e8ebaafaabe0a183403086b1d851d
davanthi <davanthijadi17@gmail.com>
2026-04-03 18:19:38 +0530

    login route

--- project/server/routes/user.route.js
+++ project/server/routes/user.route.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const User = require("../models/user.model.js");
+const bcrypt = require("bcryptjs");
 const userRouter = express.Router();
+//Register API
 userRouter.post("/register", async (req, res) => {
   try {
     const ExistingUser = await User.findOne({ email: req.body.email });
@@ -10,6 +12,10 @@ userRouter.post("/register", async (req, res) => {
         message: "user already exists with the email",
       });
     }
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = bcrypt.hashSync(req.body.password, salt);
+    req.body.password = hashPassword;
+
     // const newUser= await User.create(req.body)
     const newUser = await User(req.body);
     await newUser.save();
@@ -23,6 +29,34 @@ userRouter.post("/register", async (req, res) => {
       message: error,
     });
   }
-  //
+});
+userRouter.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      res.send({
+        success: false,
+        message: "user not found with the email please Register",
+      });
+    }
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password,
+    );
+    if (!validPassword) {
+      res.send({
+        success: false,
+        message: "sorry invalid password",
+      });
+    }
+    res.send({
+      success: true,
+      message: "user logged in successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error,
+    });
+  }
 });
 module.exports = userRouter;
