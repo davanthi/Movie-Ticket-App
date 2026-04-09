052ac71db8c0dbb794c67301d36b823493325033
davanthi <davanthijadi17@gmail.com>
2026-04-09 16:10:57 +0530

    movie api completed

--- project/server/routes/movie.route.js
+++ project/server/routes/movie.route.js
@@ -16,4 +16,68 @@ movieRouter.post("/add-movie", async (req, res) => {
     });
   }
 });
+movieRouter.put("/update-movie/:id", async (req, res) => {
+  try {
+    const movieId = req.params.id;
+    const updatedMovie = await Movie.findByIdAndUpdate(movieId, req.body);
+    res.send({
+      success: true,
+      message: "Movie updated successfully",
+      data: updatedMovie,
+    });
+  } catch (error) {
+    res.send({
+      success: false,
+      message: " server error Movie could not be updated",
+    });
+  }
+});
+movieRouter.delete("/delete-movie/:id", async (req, res) => {
+  try {
+    const movieId = req.params.id;
+    const deletedMovie = await Movie.findByIdAndDelete(movieId);
+    res.send({
+      success: true,
+      message: "movie deleted Successfully",
+      data: deletedMovie,
+    });
+  } catch (error) {
+    res.send({
+      success: false,
+      message: "server Error  movie could not be deleted",
+    });
+  }
+});
+movieRouter.get("/all-movies", async (req, res) => {
+  try {
+    const movies = await Movie.find();
+    res.send({
+      success: true,
+      message: "All movies fetched successfully",
+      data: movies,
+    });
+  } catch (error) {
+    res.send({
+      success: false,
+      message: "server error movies could not be fetched",
+    });
+  }
+});
+movieRouter.get("/:id", async (req, res) => {
+  try {
+    const movieId = req.params.id;
+    const movie = await Movie.findById(movieId);
+    res.send({
+      success: true,
+      message: "movie fetched Successfully",
+      data: movie
+    });
+  } catch (error) {
+    res.send({
+      success: false,
+      message: "server error movie could not be fetched",
+    });
+  }
+});
+
 module.exports = movieRouter;
